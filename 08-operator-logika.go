b67a1ca426e4852b0fc88680d2df0acf3ac85e03
lotsoo <braindeathrama@gmail.com>
2024-12-04 22:04:34 +0700

    feat: materi operator logika

---
+++ 08-operator-logika.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := false
+	b := true
+
+	fmt.Println("Apakah a && b?", a && b) // membandingan a && b
+	fmt.Println("Apakah a || b?", a || b) // membandingan a || b
+}
