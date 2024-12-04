6a53376bd5aa4855b636dd0bd539c4b9806f59b4
lotsoo <braindeathrama@gmail.com>
2024-12-04 22:01:17 +0700

    feat: materi operator perbandingan

---
+++ 07-operator-perbandingan.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5
+	b := 10
+	fmt.Println("Apakah a == b?", a == b) // membandingan a = b
+	fmt.Println("Apakah a != b?", a != b) // membandingan a != b
+	fmt.Println("Apakah a < b?", a < b)   // membandingan a < b
+	fmt.Println("Apakah a <= b?", a <= b) // membandingan a <= b
+	fmt.Println("Apakah a > b?", a > b)   // membandingan a > b
+	fmt.Println("Apakah a >= b?", a >= b) // membandingan a >= b
+}
